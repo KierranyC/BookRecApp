73fe9ac3ffaf21d4225e32ba72c996aa4d4d45de
KierranyC <kierranyc@outlook.com>
2024-04-16 15:05:10 -0500

    setting up...

---
+++ src/components/Books.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+
+
+const Books = ({ books, setBooks, loading, setLoading }) => {
+
+
+  useEffect(() => {
+    async function getBooks() {
+      const data = await fetchBooks()
+      console.log("BOOKS:", data)
+      setBooks(data)
+      setLoading(false)
+    }
+    getBooks()
+  }, [])
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  else {
+    return (
+      <div>
+        <h1>BOOKS</h1>
+      </div>
+    )
+  }
+
+
+}
+
+export default Books;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+import { Books } from "./components";
+
+const App = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <BrowserRouter>
+      <div id='app'>
+        <Routes>
+          <Route path='/' element={<Books books={books} setBooks={setBooks} loading={loading} setLoading={setLoading} />} />
+        </Routes>
+      </div>
+
+    </BrowserRouter>
+  )
+
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
