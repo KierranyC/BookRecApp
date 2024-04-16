6dec1c2c66304771895a495890ede8e9776a5471
KierranyC <kierranyc@outlook.com>
2024-04-16 15:47:49 -0500

    setting up...

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
-export const BASE_URL = '';
+// export const BASE_URL = '';
+const apiKey = 'AIzaSyDtRcAef-sgA69SBOXYAcdJog3cP7EI8Xg';
 
 export const fetchBooks = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/`, {
+    const genre = 'fantasy'
+    const maxResults = 3;
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=${maxResults}&key=${apiKey}`, {
       headers: {
         "Content-Type": "application/json"
       }
