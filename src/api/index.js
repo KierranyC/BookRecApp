c7b658eb8a493e186da4db532230fb170bbf5e2e
KierranyC <kierranyc@outlook.com>
2024-04-19 17:57:29 -0500

    keep searching for api with extensive list of genres/categories

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,34 @@
-// export const BASE_URL = '';
-// const apiKey = process.env.API_KEY
 import env from 'react-dotenv';
-// dotenv.config()
+
 export const fetchBooks = async () => {
   try {
-    console.log(process.env.API_KEY)
-    const genre = 'fantasy'
-    const maxResults = 3;
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=${maxResults}&key=${env.API_KEY}`, {
+
+    const categories = 'drama'
+    const maxResults = 20;
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${categories}&maxResults=${maxResults}&key=${env.API_KEY}`, {
       headers: {
         "Content-Type": "application/json"
       }
     })
 
+
     const result = await response.json()
-    console.log('API RESULTS:', result)
     return result;
   } catch (error) {
     console.error(error)
   }
 }
+
+// export const fetchBooks = async () => {
+//   try {
+
+//     const response = await fetch(`https://openlibrary.org/subjects/fiction.json?published_in=2000-2024`, {
+
+//     })
+
+//     const result = await response.json()
+//     return result;
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
